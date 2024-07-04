c936bab6694f913f3a95b11a1b54a4c229af5071
Brian Gabriel Funes <113211108+BrianFDrakmo@users.noreply.github.com>
2024-07-04 11:10:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -61,30 +61,15 @@ class Catalogo:
         self.cursor.close()
         self.cursor = self.conn.cursor(dictionary=True)
         
-#-------------------------------------------------
-# INICIA EL DESARROLLO DE LA BASE DE DATOS -------
-#-------------------------------------------------
-
-# Define una lista para almacenar los productos
-
-# Agregar un producto (create)
-
-    def agregar_producto(self, descripcion, cantidad, precio, imagen):
-
-        sql = "INSERT INTO productos (descripcion, cantidad, precio, imagen_url) VALUES (%s,%s,%s,%s)"
-        valores = (descripcion, cantidad, precio, imagen)
-        self.cursor.execute(sql, valores)
-        self.conn.commit()
-        return self.cursor.lastrowid
-
-# Consulta el código del producto
-
+    def listar_productos(self):
+        self.cursor.execute("SELECT * FROM productos")
+        productos = self.cursor.fetchall()
+        return productos
+        
     def consultar_producto(self, codigo):
         self.cursor.execute(f"SELECT * FROM productos WHERE codigo = {codigo}")
         return self.cursor.fetchone()
-
-# Mostrar por pantalla las caracteristicas del producto
-
+        
     def mostrar_producto(self, codigo):
         producto = self.consultar_producto(codigo)
         if producto:
@@ -96,42 +81,37 @@ class Catalogo:
             print(f"Imagen.....: {producto['imagen']}")
             print("-" * 50)
         else:
-            print("Producto no encontrado.")    
-        
-# Modifica un producto
+            print("Producto no encontrado.")
+            
+# (Create)
+    
+    def agregar_producto(self, descripcion, cantidad, precio, imagen):
+
+        sql = "INSERT INTO productos (descripcion, cantidad, precio, imagen_url) VALUES (%s,%s,%s,%s)"
+        valores = (descripcion, cantidad, precio, imagen)
+        self.cursor.execute(sql, valores)
+        self.conn.commit()
+        return self.cursor.lastrowid
 
     def modificar_producto(self, codigo, nueva_descripcion, nueva_cantidad, nuevo_precio, nueva_imagen):
-            sql = "UPDATE productos SET descripcion = %s, cantidad = %s, precio = %s, imagen_url = %s, WHERE codigo = %s"
-            valores = (nueva_descripcion, nueva_cantidad, nuevo_precio, nueva_imagen, codigo)
-            self.cursor.execute(sql, valores)
-            self.conn.commit()
-            return self.cursor.rowcount > 0
-
-
-
-# Muestra la lista de productos
-
-    def listar_productos(self):
-            self.cursor.execute("SELECT * FROM productos")
-            productos = self.cursor.fetchall()
-            return productos
-
-# Eliminar producto
-
+        sql = "UPDATE productos SET descripcion = %s, cantidad = %s, precio = %s, imagen_url = %s, WHERE codigo = %s"
+        valores = (nueva_descripcion, nueva_cantidad, nuevo_precio, nueva_imagen, codigo)
+        self.cursor.execute(sql, valores)
+        self.conn.commit()
+        return self.cursor.rowcount > 0
+        
     def eliminar_producto(self, codigo):
-            self.cursor.execute(f"DELETE FROM productos WHERE codigo = {codigo}")
-            self.conn.commit()
-            return self.cursor.rowcount > 0
+        self.cursor.execute(f"DELETE FROM productos WHERE codigo = {codigo}")
+        self.conn.commit()
+        return self.cursor.rowcount > 0
 
 #--------------------------------------------------------------------
 # Cuerpo del programa
 #--------------------------------------------------------------------
 # Crear una instancia de la clase Catalogo
-catalogo = Catalogo(host='juanpablocodo.mysql.pythonanywhere-services.com', user='juanpablocodo', password='root-123456', database='juanpablocodo$miapp')
+catalogo = Catalogo(host='briangfunes.mysql.pythonanywhere-services.com', user='briangfunes', password='Asdbeta23', database='briangfunes$miapp')
 
-# Carpeta para guardar las imagenes
-# ruta_destino = './static/imagenes/'
-ruta_destino = '/home/juanpablocodo/mysite/static/imagenes/'
+ruta_destino = '/home/briangfunes/mysite/static/imagenes/'
 
 @app.route("/productos", methods=["GET"])
 def listar_productos():
@@ -148,15 +128,12 @@ def mostrar_producto(codigo):
 
 @app.route("/productos", methods=["POST"])
 def agregar_producto():
-    #Recojo los datos del form
     descripcion = request.form['descripcion']
     cantidad = request.form['cantidad']
     precio = request.form['precio']
     imagen = request.files['imagen']
-    proveedor = request.form['proveedor']  
     nombre_imagen = ""
 
-    # Genero el nombre de la imagen
     nombre_imagen = secure_filename(imagen.filename)
     nombre_base, extension = os.path.splitext(nombre_imagen) 
     nombre_imagen = f"{nombre_base}_{int(time.time())}{extension}" 
@@ -170,31 +147,23 @@ def agregar_producto():
 
 @app.route("/productos/<int:codigo>", methods=["PUT"])
 def modificar_producto(codigo):
-    #Se recuperan los nuevos datos del formulario
     nueva_descripcion = request.form.get("descripcion")
     nueva_cantidad = request.form.get("cantidad")
     nuevo_precio = request.form.get("precio")
-    nuevo_proveedor = request.form.get("proveedor")
     
-    # Verifica si se proporcionó una nueva imagen
     if 'imagen' in request.files:
         imagen = request.files['imagen']
-        # Procesamiento de la imagen
         nombre_imagen = secure_filename(imagen.filename) 
         nombre_base, extension = os.path.splitext(nombre_imagen) 
         nombre_imagen = f"{nombre_base}_{int(time.time())}{extension}" 
 
-        # Guardar la imagen en el servidor
         imagen.save(os.path.join(ruta_destino, nombre_imagen))
         
-        # Busco el producto guardado
         producto = catalogo.consultar_producto(codigo)
         if producto: # Si existe el producto...
             imagen_vieja = producto["imagen_url"]
-            # Armo la ruta a la imagen
             ruta_imagen = os.path.join(ruta_destino, imagen_vieja)
 
-            # Y si existe la borro.
             if os.path.exists(ruta_imagen):
                 os.remove(ruta_imagen)
     else:     
@@ -202,23 +171,19 @@ def modificar_producto(codigo):
         if producto:
             nombre_imagen = producto["imagen_url"]
 
-   # Se llama al método modificar_producto pasando el codigo del producto y los nuevos datos.
-    if catalogo.modificar_producto(codigo, nueva_descripcion, nueva_cantidad, nuevo_precio, nombre_imagen, nuevo_proveedor):
+    if catalogo.modificar_producto(codigo, nueva_descripcion, nueva_cantidad, nuevo_precio, nombre_imagen):
         return jsonify({"mensaje": "Producto modificado"}), 200
     else:
         return jsonify({"mensaje": "Producto no encontrado"}), 403
 
 @app.route("/productos/<int:codigo>", methods=["DELETE"])
 def eliminar_producto(codigo):
-    # Primero, obtiene la información del producto para encontrar la imagen
     producto = catalogo.consultar_producto(codigo)
     if producto:
-        # Eliminar la imagen asociada si existe
         ruta_imagen = os.path.join(ruta_destino, producto['imagen_url'])
         if os.path.exists(ruta_imagen):
             os.remove(ruta_imagen)
 
-        # Luego, elimina el producto del catálogo
         if catalogo.eliminar_producto(codigo):
             return jsonify({"mensaje": "Producto eliminado"}), 200
         else:
