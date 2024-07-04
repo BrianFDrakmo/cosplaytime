25694ca00d44c2190f27a5d27245bd49a8b99f72
Brian Gabriel Funes <113211108+BrianFDrakmo@users.noreply.github.com>
2024-07-04 11:42:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ class Catalogo:
 # Crear una instancia de la clase Catalogo
 catalogo = Catalogo(host='briangfunes.mysql.pythonanywhere-services.com', user='briangfunes', password='Asdbeta23', database='briangfunes$miapp')
 
-ruta_destino = '/home/briangfunes/mysite/static/imagenes/'
+ruta_destino = '/home/briangfunes/mysite/imagenes/'
 
 @app.route("/productos", methods=["GET"])
 def listar_productos():
